5d8bba71f255f9fa6af09921f3f1b8f9c225ef3d
Syler <94995288+mrwhoseit@users.noreply.github.com>
2023-11-09 10:12:26 -0700

    fix_me.py

--- fix_me.py
+++ fix_me.py
@@ -1,61 +1,52 @@
-# Checks if a player wins the game and returns the winner
 def is_winner(game):
-
+    # Check rows, columns, and diagonals for a winner
     for i in range(3):
-        # Check rows
-        if (game[i][0] == game[i][1] == game[i][2] and game[i][0] != 0):
-            if game[i][0] == 1:
-                return 1
-            return 2
-        # Check columns
-        if (game[0][i] == game[1][i] == game[2][i] and game[0][i] != 0):
-            if game[0][i] == 1:
-                return 1
-            return 2
-        
-    # If no one wins return 0
+        if game[i][0] == game[i][1] == game[i][2] != 0 or \
+           game[0][i] == game[1][i] == game[2][i] != 0:
+            return game[i][0]
+    if game[0][0] == game[1][1] == game[2][2] != 0 or \
+       game[0][2] == game[1][1] == game[2][0] != 0:
+        return game[1][1]
     return 0
 
-# Prints the board
-def board(game):
+def print_board(game):
+    # Print the game board
     print(" --- --- --- ")
-    for i in range(3):
-        print("| " + str(game[i][0]) + " | " + str(game[i][1]) + " | " + str(game[i][2]) + " |")
+    for row in game:
+        print("| " + " | ".join(map(str, row)) + " |")
         print(" --- --- --- ")
 
-# Prompts user to make a move
 def take_move(game, player):
+    # Prompt user for a move
+    while True:
+        try:
+            x, y = map(int, input(f"Player {player}, input a move at x,y (1-3): ").split(","))
+            if 1 <= x <= 3 and 1 <= y <= 3 and game[x-1][y-1] == 0:
+                game[x-1][y-1] = player
+                break
+            else:
+                print("Invalid move.")
+        except ValueError:
+            print("Invalid input. Please enter two numbers separated by a comma.")
 
-    #Ask a given player for input
-    move = input("Player " + str(player) + ", input a move at x,y: ")
-    x,z = move.split(",")
-    x = int(x) - 1
-    z = int(z) - 1
-
-    # If the move is legal, take the move and return the board
-    if game[x][z] == 0:
-        game[x][z] = player
-        return game
-    else: 
-        print("Try Again Player " + str(player))
-        take_move(game, player)    
-
-# Run a TIc Tac Toe game
 def main():
-    # Start with an empty game board and player 1
+    # Initialize the game board
     game = [[0, 0, 0],
-	[0, 0, 0],
-	[0, 0, 0]]
+            [0, 0, 0],
+            [0, 0, 0]]
 
     player = 1
 
-    # Until someone wins, take moves and 
-    while is_winner(game):
-        game = take_move(game, player)
-        board(game)
-        print()
+    # Main game loop
+    while True:
+        print_board(game)
+        take_move(game, player)
+        winner = is_winner(game)
+        if winner != 0:
+            print_board(game)
+            print(f"Player {winner} wins!")
+            break
+        player = 3 - player  # Switch player (1 -> 2, 2 -> 1)
 
-    print("Player " + str(is_winner(game)) + " wins!")
-
-
-main()
+if __name__ == "__main__":
+    main()
